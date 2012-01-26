8e3b8c3043db14b4ec4c71be608a9e04233ada02
Fady <fady@fkportfolio.com>
2012-01-26 23:34:02 +1100

    added preset config params

--- config/imagefly.php
+++ config/imagefly.php
@@ -11,4 +11,8 @@ return array
     'cache_expire'     => 7 * 24 * 60 * 60,
     'cache_dir'        => 'media/cache/',
     'mimic_source_dir' => TRUE,
+    'enforce_presets'  => TRUE,
+    'presets'          => array(
+        'w320-h240-c',
+    ),
 );
