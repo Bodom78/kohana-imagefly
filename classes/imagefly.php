22084ec8f1272109422a1fc1f5873183b9cbb855
Fady Khalife <fady@fkportfolio.com>
2012-01-27 01:51:22 +1100

    fixed spelling

--- classes/imagefly.php
+++ classes/imagefly.php
@@ -151,7 +151,7 @@ class ImageFly
         $params = Request::current()->param('params');
         $filepath = Request::current()->param('imagepath');
         
-        // If enforcing params, enssure it's a match
+        // If enforcing params, ensure it's a match
         if ($this->config['enforce_presets'] AND ! in_array($params, $this->config['presets']))
             throw new HTTP_Exception_404('The requested URL :uri was not found on this server.',
                                                     array(':uri' => Request::$current->uri()));
